acbbcbc27e03618bffcd42661d192ce36def30ec
imyzt <imyzt01@email.com>
2018-03-30 20:45:40 +0800

    main方法

---
+++ src/main/java/top/imyzt/itcase/day07/Main.java
@@ -0,0 +1,20 @@
+package top.imyzt.itcase.day07;
+
+/**
+ * main方法
+ * @author 01
+ *
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		/*
+		 * public 被jvm调用,所以权限要足够大
+		 * static 被jvm调用,不需要创建对象,直接类名调用即可
+		 * void 被jvm调用,不需要有任何返回值
+		 * main 只有这样写才能被jvm识别,main不是关键字
+		 * String[] args 以前用来接收键盘录入的
+		 */
+	}
+
+}

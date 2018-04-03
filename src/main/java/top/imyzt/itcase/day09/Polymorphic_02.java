019ac8c6276e5609174084fdd4883925a1cf74bf
imyzt <imyzt01@gmail.com>
2018-04-03 23:02:07 +0800

    多态中的成员方法和静态方法运行时和编译时的状态

--- src/main/java/top/imyzt/itcase/day09/Polymorphic_02.java
+++ src/main/java/top/imyzt/itcase/day09/Polymorphic_02.java
@@ -3,22 +3,49 @@ package top.imyzt.itcase.day09;
 /**
  * 
  * 成员变量
- * 		编译看左边(父类), 运行看左边(父类)
+ * 		编译看左边(父类),运行看左边(父类)
+ * 成员方法
+ * 		编译看左边(父类),运行看右边(子类)
+ * 静态方法
+ * 		编译看左边(父类),运行看左边(父类)
+ * 		静态方法和成员变量同属于类
  * @author 01
  */
 public class Polymorphic_02 {
 
 	public static void main(String[] args) {
 		Fu01 f = new Zi01();
-		//f的指针只能操作new Zi01()中super区(父)中的成员变量
+		//f的指针只能操作new Zi01()中super区(父)中的成员变量(因为左边Fu01 f是Fu01类型的,故只能操作super区父类的成员变量)
 		System.out.println(f.num);		//10
+		//f的指针能操作重写了的New Zi01()中的print()方法(因为动态绑定机制,编译检查父类是否存在该方法保证编译正常进行,运行动态绑定子类的此方法)
+		f.print();
+		//static method 和成员变量同属于类,所以和成员变量一样,只能操作到super区的方法
+		f.say();
 	}
 }
 
 class Fu01{
 	int num = 10;
+	
+	public void print() {
+		System.out.println("Fu01");
+	}
+	
+	public static void say() {
+		System.out.println("Fu01 static");
+	}
 }
 
 class Zi01 extends Fu01{
 	int num = 20;
+	
+	@Override
+	public void print() {
+		System.out.println("Zi01");
+	}
+	
+	//此处禁止使用 @Override 注解
+	public static void say() {
+		System.out.println("Zi01 static");
+	}
 }

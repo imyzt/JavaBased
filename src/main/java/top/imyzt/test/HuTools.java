e65d0026d9c15a31bee322d980ca9028098e4aaa
imyzt <imyzt01@email.com>
2018-04-24 16:40:20 +0800

    学习hutool的常用方法

---
+++ src/main/java/top/imyzt/test/HuTools.java
@@ -0,0 +1,29 @@
+package top.imyzt.test;
+
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.IdcardUtil;
+
+/** 
+* @author 杨镇涛
+* @date 2018年4月24日 上午9:13:14 
+* @version 1.0 
+* @Description 
+*/
+public class HuTools {
+
+
+	public static void main(String[] args) {
+		String idCard = "321083197812162119";
+		System.out.println(IdcardUtil.isValidCard("321083197812162119"));
+		System.out.println(IdcardUtil.hide("321083197812162119", 6, 14));
+		DateTime parse = DateUtil.parse("2018-04-24");
+		System.out.println(IdcardUtil.getAgeByIdCard(idCard, parse));
+	}
+
+}
+
+class Person{
+	private String name;
+}

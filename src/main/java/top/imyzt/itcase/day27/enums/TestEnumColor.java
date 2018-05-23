2fbf868f3b5eec6eba3be330f8f2456f96a137b1
imyzt <imyzt01@email.com>
2018-05-23 14:36:15 +0800

    jdk1.7的新特性

--- src/main/java/top/imyzt/itcase/day27/enums/TestEnumColor.java
+++ src/main/java/top/imyzt/itcase/day27/enums/TestEnumColor.java
@@ -9,15 +9,31 @@ package top.imyzt.itcase.day27.enums;
  * @date 2018年5月23日 上午10:44:13 
  */
 public class TestEnumColor {
-	
+
 	public static void main(String[] args) {
+
+		//返回枚举的编号
+		System.out.println(Color.RED.ordinal());
+
+		//比较的是编号
+		System.out.println(Color.RED.compareTo(Color.BLANK));
+
+		//获取实例名称
+		System.out.println(Color.RED.name());
+
+		//获取实例名称
+		System.out.println(Color.BLANK.toString());
+
+		//通过字节码对象获取枚举项
+		Season value = Season.valueOf(Season.class, "SPRING");
+		System.out.println(value);
 		
-		/*for (Color c : Color.values()) {
-			System.out.println(c);
-		}*/
-	
-		
-		System.out.println(Season.SPRING.show());
+		//获取所有枚举项
+		for (Color c : Color.values()) {
+			System.out.print(c + "，");
+		}
+
+//		System.out.println(Season.SPRING.show());
 	}
 
 }
